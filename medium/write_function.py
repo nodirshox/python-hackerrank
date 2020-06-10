40f4d17e2f732e427fcfa721e23e4913324dbb30
nodirshox <n.ergashev@student.inha.uz>
2020-06-10 19:30:43 +0500

    Add Write Function problem

---
+++ medium/write_function.py
@@ -0,0 +1,14 @@
+def is_leap(year):
+    leap = False
+
+    # Check logis
+    if year % 4 == 0:
+        leap = True
+        if year % 100 == 0 and year % 400:
+            leap = False
+    
+    return leap
+year = int(input())
+
+result = is_leap(year)
+print(result)
