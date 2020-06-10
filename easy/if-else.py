b6471e93071647c7e33eca20a0aa9a1278c3d780
nodirshox <n.ergashev@student.inha.uz>
2020-06-10 15:15:47 +0500

    Add If-Else problem

---
+++ easy/if-else.py
@@ -0,0 +1,23 @@
+"""
+https://www.hackerrank.com/challenges/py-if-else/problem
+"""
+number = input("Enter a number: ")
+
+# Try conver input to integer, otherwise say alert and quit
+try:
+    number = int(number)
+except:
+    print("Please, enter a number.")
+    quit()
+
+if number % 2 == 1:
+    print('Weird')
+else:
+    if number >=2 and number <=5:
+        print('Not Weird')
+    elif number >=6 and number <=20:
+        print('Weird')
+    elif number > 20:
+        print('Not Weird')
+    else:
+        print('Not in range')
